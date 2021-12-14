627a73408493c0d625f81caa40921219b338cf12
mitul-121 <63252539+mitul-121@users.noreply.github.com>
2021-12-14 22:16:49 +0200

    goi

---
+++ Function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/* function declaration */
+void print_big(int number){
+    if(number > 10)
+        printf("%d is big\n");
+}
+
+int main() {
+  int array[] = { 1, 11, 2, 22, 3, 33 };
+  int i;
+  for (i = 0; i < 6; i++) {
+    print_big(array[i]);
+  }
+  return 0;
+}
