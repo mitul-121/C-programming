667e626a345d71ad900b9229aeda5bf143bd95cb
mitul-121 <63252539+mitul-121@users.noreply.github.com>
2021-12-17 18:58:49 +0200

    Add files via upload

--- dijkstras.c
+++ dijkstras.c
@@ -10,7 +10,19 @@ int main()
 
     int vertex[a][a-1];
     int dist[a][a-1];
-    int nu[a-1];
+    int nu[a+1];
+    int prec[a][a-1];
+    int distance[a][a-1];
+
+
+    for(int i =1; i<=a; i++)
+    {
+        for(int j =1; j<=a;j++){
+            distance[i,j] == 100;
+        }
+
+
+    }
 
 
     for(int i=1; i<a+1;i++){
@@ -20,7 +32,7 @@ int main()
 
         printf("\nEnter the connected vertexes: \n");
 
-        for(int j =1; j<nu[i]+1; j++)
+        for(int j=1; j<nu[i]+1; j++)
         {
             scanf("%d", &vertex[i][j]);
 
@@ -69,6 +81,50 @@ int main()
      }
      printf("\n----------------------------\n");
     }
+    int start;
+    printf("Enter the number of starting vertex ");
+        scanf("%d",&start);
+
+
+
+    prec[1][start] = start;
+    distance[1][start] = 0;
+
+
+     int min = distance[1][vertex[start][1]];
+
+    for(int j=1; j< 10;j++){
+        for(int i=1; i<=nu[start];i++){
+            if(distance[1][vertex[start][i]] >  min + distance[1][vertex[start][i]] ){
+                distance[1][vertex[start][i]] = min + distance[1][vertex[start][i]];
+
+        }
+        }
+
+
+    for(int i =1; i <= nu[start]; i++)
+    {
+        prec[1][vertex[start][i]] = start;
+        distance[1][vertex[start][i]] = dist[start,i];
+    }
+
+
+    for(int i =1; i <= nu[start]; i++)
+    {
+
+        if(distance[1][vertex[start][i]] < min ){
+            min = distance[1][vertex[start][i]];
+        }
+
+    }
+    }
+    for(int j=1; j< 10;j++){
+            printf("%d ",prec[1][j]);
+            printf("%d ",distance[1][j]);
+
+
+}
+
 
 
 
